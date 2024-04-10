901c24dd6a51e505d7bccc2480ad2974c6f95fa7
sreejaya <sreejayam16@gmail.com>
2024-04-10 12:52:54 +0530

    loginlogoutreg

--- Ekart-Restuarant-main/Ekartproject/Ekartproject/urls.py
+++ Ekart-Restuarant-main/Ekartproject/Ekartproject/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.registerPage, name="register"),
+    path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),
 	path('logout/', views.logoutUser, name="logout"),
 
