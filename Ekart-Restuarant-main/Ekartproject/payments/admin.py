21b09ab2abf6da4cf428a7d8449c08e20c102e43
sreejaya <sreejayam16@gmail.com>
2024-05-14 09:16:24 +0530

    ekart project

--- Ekart-Restuarant-main/Ekartproject/payments/admin.py
+++ Ekart-Restuarant-main/Ekartproject/payments/admin.py
@@ -1,8 +1,8 @@
-# admin.py
-
-from django.contrib import admin
-from .models import Order
-
-
-admin.site.register(Order)
-
+# # admin.py
+#
+# from django.contrib import admin
+# from .models import Order
+#
+#
+# admin.site.register(Order)
+#
